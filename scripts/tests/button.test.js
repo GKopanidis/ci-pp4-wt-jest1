3f5ce4d62575f492987a1e6936fa7780adf6d464
Georgios Kopanidis <kopanidis@me.com>
2023-12-29 11:03:21 +0000

    Add h1 test to button.test.js

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,13 +5,19 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+   let fs = require("fs");
+   let fileContents = fs.readFileSync("index.html", "utf-8");
+   document.open();
+   document.write(fileContents);
+   document.close();
 });
 
 describe("DOM tests", () => {
     test("Expects content to change", () => {
         buttonClick();
-        expect(document.getElementById("par")
-            .innerHTML).toEqual("You Clicked");
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
